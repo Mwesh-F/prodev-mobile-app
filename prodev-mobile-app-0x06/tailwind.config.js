9be6f26d0d663c61705072cdbd775125f6a362c4
Frida Mwende <mwendehfrida@gmail.com>
2025-07-19 19:00:56 +0300

    Add files via upload

---
+++ prodev-mobile-app-0x06/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./styles/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#34967C',
+        secondary: '#0601B4',
+      },
+    },
+  },
+  plugins: [],
+}; 
